dd8a3ca510e5000a691964a2866dd517a2adede5
mabakhit22 <Mariam.20377305.aun.edu.eg>
2021-10-20 15:21:07 +0200

    first commit

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	int x, y;
+	cout << "please enter two mun" << endl;
+	cin >> x;
+	cin >> y;
+	int z = x + y;
+	cout << z << endl;
+	system("pause");
+}
